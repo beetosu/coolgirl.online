f2bdc8b34eb1d6f4ee6642b82a5a17502ee3c332
beetosu <beetosu@gmail.com>
2025-12-04 18:48:19 -0500

    start styling index page

--- components/blog-list.js
+++ components/blog-list.js
@@ -1,7 +1,27 @@
 class BlogList extends HTMLElement {
     constructor() {
         super();
-        const shadow = this.attachShadow({ mode: "open" });
+        const shadow = this.attachShadow({ mode: "open" }),
+            sheet = new CSSStyleSheet();
+        
+        sheet.replaceSync(`
+            align-self: end;
+            
+            h2 {
+                font-size: 20px;
+                margin: 0;
+            }
+
+            ul {
+                margin: 0;
+
+                a { 
+                    color: initial; 
+                }
+            }
+        `);
+        shadow.adoptedStyleSheets = [ sheet ];
+        
         fetch('metadata.json')
             .then(response => {
                 if (!response.ok) {
@@ -9,23 +29,29 @@ class BlogList extends HTMLElement {
                 }
                 return response.json(); // Parse the JSON data
             })
-            .then(blogs => {
-                if (!Object.keys(blogs).length) return;
-
-                const title = document.createElement('h2');
-                title.textContent = 'Recent Posts:';
-                shadow.appendChild(title);
-
-                const ul = document.createElement('ul');
-                Object.values(blogs).forEach(blog => {
-                    const li = document.createElement('li');
-                    li.textContent = blog.date ? `${blog.date} - ${blog.title}` : blog.title; // Assuming each blog object has a "title" property
-                    ul.appendChild(li);
-                });
-
-                shadow.appendChild(ul);
-            });
+            .then(blogs => buildBloglist(blogs, shadow));
     }
+}
+
+function buildBloglist(blogs, shadow) {
+    if (!Object.keys(blogs).length) return;
+
+    const title = document.createElement('h2');
+    title.textContent = 'Recent Posts:';
+    shadow.appendChild(title);
+
+    const ul = document.createElement('ul');
+    Object.entries(blogs).forEach(([url, blog]) => {
+        const li = document.createElement('li'),
+            link = document.createElement('a');
+        
+        link.href = `/blog/${url}.html`
+        link.textContent = blog.date ? `${blog.date} - ${blog.title}` : blog.title; // Assuming each blog object has a "title" property
+        li.appendChild(link);
+        ul.appendChild(li);
+    });
+
+    shadow.appendChild(ul);
 }
 
 customElements.define("blog-list", BlogList);
