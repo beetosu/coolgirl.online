5d02cc790fe472e866f2c4b269058ec90985edd7
beetosu <beetosu@gmail.com>
2025-12-04 12:46:22 -0500

    use my handy dandy meta object

--- components/blog-list.js
+++ components/blog-list.js
@@ -1,9 +1,30 @@
 class BlogList extends HTMLElement {
-    connectedCallback() {
+    constructor() {
+        super();
         const shadow = this.attachShadow({ mode: "open" });
-        this.shadowRoot.innerHTML = `
-            <p>hi</p>
-        `
+        fetch('metadata.json')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json(); // Parse the JSON data
+            })
+            .then(blogs => {
+                if (!Object.keys(blogs).length) return;
+
+                const title = document.createElement('h2');
+                title.textContent = 'Recent Posts:';
+                shadow.appendChild(title);
+
+                const ul = document.createElement('ul');
+                Object.values(blogs).forEach(blog => {
+                    const li = document.createElement('li');
+                    li.textContent = blog.date ? `${blog.date} - ${blog.title}` : blog.title; // Assuming each blog object has a "title" property
+                    ul.appendChild(li);
+                });
+
+                shadow.appendChild(ul);
+            });
     }
 }
 
